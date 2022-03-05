a3b08b7a4bcd96f3f87099b5a5e076a7920e7f69
toanelnino1902 <toan.nq194386@gmail.com>
2022-03-05 21:21:39 +0700

    tao nhanh feature/header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import './Header.css';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+        <p className='Header'>This is header</p>
+    </React.Fragment>
+  );
+};
+
+export default Header;
