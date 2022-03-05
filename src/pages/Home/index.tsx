ff8bb38cd1e18d63e7ca53881faed7cc40821717
toanelnino1902 <toan.nq194386@gmail.com>
2022-03-05 21:19:18 +0700

    tao feature/header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from 'src/components/Header';
 
 const HomePage: React.FC = () => {
   return (
     <React.Fragment>
+      <Header/>
       <div>Dit me may</div>
     </React.Fragment>
   );
