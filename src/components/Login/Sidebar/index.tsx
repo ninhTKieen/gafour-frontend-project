68e1cfa6ba778d651b0b96c654fcb139f710d485
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-08 21:10:07 +0700

    fix login css

--- src/components/Login/Sidebar/index.tsx
+++ src/components/Login/Sidebar/index.tsx
@@ -6,8 +6,11 @@ const LoginSideBar: React.FC = () => {
   return (
     <React.Fragment>
       <div className="login-side-bar">
-        <div className="login-side-bar-web-title">
-          Gafour
+        <div className="login-side-bar-web-title">Gafour</div>
+
+        <div className="login-side-bar-body">
+          <div className="login-side-bar-body-title">Xin chào Gafourer!</div>
+          <div className="login-side-bar-body-number">Số điện thoại</div>
         </div>
       </div>
     </React.Fragment>
