a41431a664e352f8d13557a3b17f13e4170ed339
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-14 23:22:56 +0700

    add login intro

---
+++ src/components/Login/Intro/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './LoginIntro.css';
+
+const LoginIntro: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div className="login-intro">
+        <span className="login-intro-title">Công ty cổ phần công nghệ sản xuất Chuối</span>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default LoginIntro;
