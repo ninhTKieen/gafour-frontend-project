509d1107a196cbe3ba0b9e5d37ca72cc858d4b94
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-14 22:47:27 +0700

    add sign in by gmail option

--- src/components/Login/Sidebar/index.tsx
+++ src/components/Login/Sidebar/index.tsx
@@ -9,6 +9,7 @@ import {
   Button,
 } from '@chakra-ui/react';
 import flag from 'src/assets/icons/vietnam-flag.png';
+import { SiGmail as Gmail } from 'react-icons/si';
 
 import './LoginSidebar.css';
 
@@ -19,30 +20,33 @@ const LoginSideBar: React.FC = () => {
   return (
     <React.Fragment>
       <div className="login-side-bar">
-        <div className="login-side-bar-web-title">Gafour</div>
+        <div className="login-side-bar-main">
+          <div className="login-side-bar-web-title">Gafour</div>
 
-        <div className="login-side-bar-body">
-          <div className="login-side-bar-body-title">Xin chào Gafourer!</div>
-          <div className="login-side-bar-body-number">Số điện thoại</div>
+          <div className="login-side-bar-body">
+            <div className="login-side-bar-body-title">Xin chào Gafourer!</div>
+            <div className="login-side-bar-body-number">Số điện thoại</div>
 
-          <div className="login-side-bar-body-input">
-            <Stack>
-              <InputGroup size="lg" marginBottom="16px">
-                <InputLeftAddon>
-                  <img src={flag} style={{ width: 20, height: 'auto', marginRight: 5 }} />
-                  +84
-                </InputLeftAddon>
-                <Input type="tel" placeholder="Nhập sđt" />
-              </InputGroup>
-              <InputGroup size="lg" marginBottom="16px">
-                <Input type={show ? 'text' : 'password'} placeholder="Nhâp mật khẩu" />
+            <div className="login-side-bar-body-input">
+              <Stack>
+                <InputGroup size="lg" marginBottom="16px">
+                  <InputLeftAddon>
+                    <img src={flag} style={{ width: 20, height: 'auto', marginRight: 5 }} />
+                    +84
+                  </InputLeftAddon>
+                  <Input type="tel" placeholder="Nhập sđt" />
+                </InputGroup>
 
-                <InputRightElement width="72px">
-                  <Button h="28px" size="sm" onClick={() => setShow(!show)}>
-                    {show ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
+                <InputGroup size="lg" marginBottom="16px">
+                  <Input type={show ? 'text' : 'password'} placeholder="Nhâp mật khẩu" />
+
+                  <InputRightElement width="72px">
+                    <Button h="28px" size="sm" onClick={() => setShow(!show)}>
+                      {show ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </Stack>
 
               <Input
                 type="submit"
@@ -52,8 +56,21 @@ const LoginSideBar: React.FC = () => {
                 height="52px"
                 width="100%"
                 disabled={true}
+                marginTop="24px"
+                color="white"
               />
-            </Stack>
+
+              <Button
+                leftIcon={<Gmail />}
+                width="100%"
+                bg="red"
+                color="white"
+                marginTop="20px"
+                height="52px"
+              >
+                Đăng nhập bằng Gmail
+              </Button>
+            </div>
           </div>
         </div>
       </div>
