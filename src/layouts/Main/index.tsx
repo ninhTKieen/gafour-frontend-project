1d86670fadf93773e1296b03b3d7d9cf74074346
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-15 23:12:31 +0700

    add main layout

---
+++ src/layouts/Main/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ToastContainer } from 'react-toastify';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout: React.FC = () => {
+  return (
+    <React.Fragment>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+      />
+      <Outlet />
+      {/* to render child element route */}
+    </React.Fragment>
+  );
+};
+
+export default MainLayout;
