a61eef3f8756a1d87c94d6bee0abd210aaf571d1
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-15 11:32:17 +0700

    add login content

--- src/components/Login/Intro/index.tsx
+++ src/components/Login/Intro/index.tsx
@@ -6,7 +6,18 @@ const LoginIntro: React.FC = () => {
   return (
     <React.Fragment>
       <div className="login-intro">
-        <span className="login-intro-title">Công ty cổ phần công nghệ sản xuất Chuối</span>
+        <div className="login-intro-body">
+          <span className="login-intro-title">Công ty Cổ phần công nghệ sản xuất Chuối</span>
+          <div className="login-intro-content-wrapper">
+            <span className="login-intro-content">
+              Địa chỉ: Số 1 Đại Cồ Việt, Bách Khoa, Hai Bà Trưng, Hà Nội
+            </span>
+            <span className="login-intro-content">
+              SĐT: 0933699918 
+            </span>
+            <span className='login-intro-content'>Email: theinvincibleUnited@gmail.com</span>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
