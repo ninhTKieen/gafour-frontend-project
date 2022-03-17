ed3aa8005550426ad69b2123cfd8489a0c001df3
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-17 12:52:57 +0700

    add header searchbar

---
+++ src/components/Header/SearchBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Stack, InputLeftElement, InputGroup, Input } from '@chakra-ui/react';
+import { GoSearch as SearchIcon } from 'react-icons/go';
+
+const HeaderSearchBar: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Stack spacing={4}>
+        <InputGroup w="100%">
+          <InputLeftElement
+            // eslint-disable-next-line react/no-children-prop
+            children={<SearchIcon color="grey.300" />}
+            bg="transparent"
+            cursor="pointer"
+          />
+          <Input
+            type="tel"
+            placeholder="Tìm kiếm"
+            borderRadius="20px"
+            // _focus={{ bg: '#eee'}}
+            focusBorderColor="#79bf43"
+          />
+        </InputGroup>
+      </Stack>
+    </React.Fragment>
+  );
+};
+
+export default HeaderSearchBar;
