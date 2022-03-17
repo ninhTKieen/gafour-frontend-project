cb241542e9561e6476b785fa44cfb685cf1d3901
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-17 13:01:34 +0700

    add login route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import MainLayout from 'src/layouts/Main';
 
 import HomePage from 'src/pages/Home';
+import LoginPage from 'src/pages/Login';
 
 const routes = [
   {
     path: '/',
     element: <MainLayout />,
-    children: [{ path: '/', element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/login', element: <LoginPage /> },
+    ],
   },
 ];
 
