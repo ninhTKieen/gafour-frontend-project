8cb6a231797e34eff2d558d518bd45c182e43ce0
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-18 12:57:15 +0700

    fuck react

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
-import { Flex, Box, Tabs, TabList, Tab } from '@chakra-ui/react';
+
+import { Flex, Box, Tabs, TabList, Tab, IconButton } from '@chakra-ui/react';
+
+import BadgeIcon from 'src/components/commons/BadgeIcon';
+
+import { AiOutlineHome as HomeIcon } from 'react-icons/ai';
+import { HiOutlineChatAlt2 as ChatIcon } from 'react-icons/hi';
 
 const HeaderNavBar: React.FC = () => {
   return (
-    <Flex w="60%" h="100%" >
+    <Flex w="60%" h="100%">
       <Box w="100%" h="100%">
-        <Tabs align="center" variant="line" colorScheme="green">
+        <Tabs align="center" variant="line" colorScheme="green" isFitted>
           <TabList>
             <Tab
               h="100%"
-              p="28px"
+              p="12px"
               // onClick={() => console.log('this is tab 1')}
               _focus={{ boxShadow: 'none' }}
             >
-              One
+              <IconButton
+                aria-label="notification"
+                icon={<HomeIcon size="30" />}
+                bg="transparent"
+                isRound
+                size="lg"
+              />
             </Tab>
             <Tab
               h="100%"
-              p="28px"
+              p="12px"
               // onClick={() => console.log('this is tab 2')}
               _focus={{ boxShadow: 'none' }}
             >
-              Two
+              <IconButton
+                aria-label="notification"
+                icon={
+                  <BadgeIcon
+                    // eslint-disable-next-line react/no-children-prop
+                    children={<ChatIcon size={32} />}
+                  />
+                }
+                bg="transparent"
+                isRound
+                size="lg"
+              />
             </Tab>
           </TabList>
         </Tabs>
