3d6f6550cc51f362babef753f998b06f365b7464
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-18 12:12:29 +0700

    add profile icon

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Notification from './Notification';
 import Friends from './Friends';
+import ProfileIcon from './ProfileIcon';
 
 import { Flex, HStack } from '@chakra-ui/react';
 
@@ -9,9 +10,10 @@ const HeaderMenu: React.FC = () => {
   return (
     <React.Fragment>
       <Flex w="20%" align="center">
-        <HStack spacing={10} marginLeft={10}>
+        <HStack spacing={10}>
           <Notification />
           <Friends />
+          <ProfileIcon />
         </HStack>
       </Flex>
     </React.Fragment>
