55ff943a5d36a7fd82d8d8ee047cd36a62c55be4
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-18 21:54:10 +0700

    add settings route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import MainLayout from 'src/layouts/Main';
 
 import HomePage from 'src/pages/Home';
+import SettingsPage from 'src/pages/Settings';
 
 const routes = [
   {
     path: '/',
     element: <MainLayout />,
-    children: [{ path: '/', element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/settings', element: <SettingsPage /> },
+    ],
   },
 ];
 
