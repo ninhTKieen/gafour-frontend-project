7bd508a546db3759ac54945f0d816e3beffb2a3e
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-19 20:16:10 +0700

    Create Setting page template

---
+++ src/layouts/Settings/Template.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Header from 'src/components/Header';
+import SettingsBox from 'src/components/Settings/Box';
+
+import { Flex } from '@chakra-ui/react';
+
+const SettingsTemplate: React.FC = (props) => {
+  return (
+    <React.Fragment>
+      <div style={{ backgroundColor: '#f2f2f2', height: '100vh' }}>
+        <Header />
+        <Flex className="settings-page" >
+          <SettingsBox />
+          {props.children}
+        </Flex>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default SettingsTemplate;
