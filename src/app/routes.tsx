e505913479551c567df8e82286fd65b44838c96d
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-19 21:55:31 +0700

    Add routes for settings page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,14 +3,30 @@ import MainLayout from 'src/layouts/Main';
 
 import HomePage from 'src/pages/Home';
 import SettingsPage from 'src/pages/Settings';
+import SettingsGeneralPage from 'src/pages/Settings/General';
 
-const routes = [
+const routes: {
+  path: string;
+  element: JSX.Element;
+  children: {
+    path: string;
+    element: JSX.Element;
+  }[];
+}[] = [
   {
     path: '/',
     element: <MainLayout />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/settings', element: <SettingsPage /> },
+    ],
+  },
+  {
+    path: '/settings',
+    element: <MainLayout />,
+    children: [
+      { path: '', element: <SettingsPage /> },
+      { path: 'general', element: <SettingsGeneralPage /> },
     ],
   },
 ];
