4f4639c67d0d3ab549de204148d06637cfa026d4
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-03-22 16:16:33 +0700

    Update page background and tab header navigate

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { Flex, Box, Tabs, TabList, Tab, IconButton } from '@chakra-ui/react';
 
@@ -10,47 +10,40 @@ import { AiOutlineHome as HomeIcon } from 'react-icons/ai';
 import { HiOutlineChatAlt2 as ChatIcon } from 'react-icons/hi';
 
 const HeaderNavBar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <Flex w="60%" h="100%">
       <Box w="100%" h="100%">
         <Tabs align="center" variant="line" colorScheme="green" isFitted>
           <TabList>
-            <Tab
-              h="100%"
-              p="12px"
-              // onClick={() => console.log('this is tab 1')}
-              _focus={{ boxShadow: 'none' }}
-            >
-              <Link to="/">
-                <IconButton
-                  aria-label="notification"
-                  icon={<HomeIcon size={24} />}
-                  bg="transparent"
-                  isRound
-                  size="lg"
-                />
-              </Link>
+            <Tab h="100%" p="12px" onClick={() => navigate('/')} _focus={{ boxShadow: 'none' }}>
+              <IconButton
+                aria-label="notification"
+                icon={<HomeIcon size={24} />}
+                bg="transparent"
+                isRound
+                size="lg"
+              />
             </Tab>
             <Tab
               h="100%"
               p="12px"
-              // onClick={() => console.log('this is tab 2')}
+              onClick={() => navigate('/messenger')}
               _focus={{ boxShadow: 'none' }}
             >
-              <Link to="/messenger">
-                <IconButton
-                  aria-label="notification"
-                  icon={
-                    <BadgeIcon
-                      // eslint-disable-next-line react/no-children-prop
-                      children={<ChatIcon size={24} />}
-                    />
-                  }
-                  bg="transparent"
-                  isRound
-                  size="lg"
-                />
-              </Link>
+              <IconButton
+                aria-label="notification"
+                icon={
+                  <BadgeIcon
+                    // eslint-disable-next-line react/no-children-prop
+                    children={<ChatIcon size={24} />}
+                  />
+                }
+                bg="transparent"
+                isRound
+                size="lg"
+              />
             </Tab>
           </TabList>
         </Tabs>
